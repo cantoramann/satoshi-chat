8a3a25b7a15564e5e9adc2f6547c2d2a91a99fee
Can Toraman <toraman@usc.edu>
2023-01-16 11:07:11 -0800

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 import "../styles/fonts.css";
 
@@ -5,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <div className="bg-black min-h-screen text-white font-noto">
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
